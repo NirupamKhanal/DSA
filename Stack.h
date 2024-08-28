866fd5074ba6fbada9c7b16cf4184985c9c5eccf
Nirupam Khanal <134681775+NirupamKhanal@users.noreply.github.com>
2024-08-28 10:30:52 -0500

    Add files via upload

---
+++ Stack.h
@@ -0,0 +1,28 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <string>
+
+using namespace std;
+
+class Stack{
+    private:
+
+        struct item {
+            string name; 
+            int value; 
+            item* prev;
+        };
+        item* stackPtr;
+
+    public: 
+        void Push(string name, int value);
+        void Pop();
+        void ReadItem(item* r);
+        void Print();
+
+        Stack();
+        ~Stack();
+};
+
+#endif
